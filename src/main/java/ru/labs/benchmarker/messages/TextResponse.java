5815868b866633e174d152d41228b0d65cbaff5f
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-10 23:55:16 +0300

    Scripted auto-commit on change (2019-11-10 23:55:16) by gitwatch.sh

--- src/main/java/ru/labs/benchmarker/messages/TextResponse.java
+++ src/main/java/ru/labs/benchmarker/messages/TextResponse.java
@@ -1,4 +1,21 @@
-package ru.labs.benchmarker.messages;
+package ru.labs.jstester.messages.HttpResponses;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class TextResponse {
+    private String message;
+
+    public TextResponse() {
+        this.message = "";
+    }
+
+    public TextResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
