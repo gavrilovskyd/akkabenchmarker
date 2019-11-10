fa32541ec04e83acd89b1f265d1e6cc2ee558ed7
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-10 23:25:20 +0300

    Scripted auto-commit on change (2019-11-10 23:25:20) by gitwatch.sh

--- src/main/java/ru/labs/benchmarker/actors/CacheActor.java
+++ src/main/java/ru/labs/benchmarker/actors/CacheActor.java
@@ -13,7 +13,9 @@ public class CacheActor extends AbstractActor {
     private LinkedHashMap<String, Long> lruCache;
 
     public CacheActor(int cacheSize) {
-        this.lruCache = new LinkedHashMap<>(cacheSize, 0.75f, true);
+        this.lruCache = new LinkedHashMap<>(cacheSize, 0.75f, true) {
+            
+        };
     }
 
     @Override
