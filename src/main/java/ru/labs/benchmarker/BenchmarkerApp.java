56e836437926b3efd212d472c256c31006b3d7dc
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-10 18:05:00 +0300

    Scripted auto-commit on change (2019-11-10 18:05:00) by gitwatch.sh

--- src/main/java/ru/labs/benchmarker/BenchmarkerApp.java
+++ src/main/java/ru/labs/benchmarker/BenchmarkerApp.java
@@ -5,5 +5,7 @@ import akka.actor.ActorSystem;
 public class BenchmarkerApp {
     public static void main(String[] args) throws Exception {
         ActorSystem system = ActorSystem.create("benchmarker-system");
+
+
     }
 }
