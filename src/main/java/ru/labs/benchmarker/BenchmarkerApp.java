cb14df11e34e71436f426ff9ae90b21b47e13173
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-10 18:05:36 +0300

    Scripted auto-commit on change (2019-11-10 18:05:36) by gitwatch.sh

--- src/main/java/ru/labs/benchmarker/BenchmarkerApp.java
+++ src/main/java/ru/labs/benchmarker/BenchmarkerApp.java
@@ -1,11 +1,13 @@
 package ru.labs.benchmarker;
 
 import akka.actor.ActorSystem;
+import akka.http.javadsl.Http;
 
 public class BenchmarkerApp {
     public static void main(String[] args) throws Exception {
         ActorSystem system = ActorSystem.create("benchmarker-system");
 
-
+        final Http http = Http.get(system);
+        final 
     }
 }
