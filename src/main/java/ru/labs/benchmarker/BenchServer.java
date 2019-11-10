bf81df3a5cbf5672441ff507ea969b1bed50c699
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-10 22:10:40 +0300

    Scripted auto-commit on change (2019-11-10 22:10:40) by gitwatch.sh

--- src/main/java/ru/labs/benchmarker/BenchServer.java
+++ src/main/java/ru/labs/benchmarker/BenchServer.java
@@ -89,6 +89,6 @@ public class BenchServer {
                                     .thenCompose(response ->
                                             CompletableFuture.completedFuture(System.nanoTime() - start));
                         });
-        Sink<Long, CompletionStage<Long>> 
+        Sink<Long, CompletionStage<Long>> sumFold = Sink.fold(0, )
     }
 }
