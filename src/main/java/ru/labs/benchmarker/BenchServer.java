59ec51a725d25de6c8fa9f2ead64478695e1c2e4
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-10 19:57:28 +0300

    Scripted auto-commit on change (2019-11-10 19:57:28) by gitwatch.sh

--- src/main/java/ru/labs/benchmarker/BenchServer.java
+++ src/main/java/ru/labs/benchmarker/BenchServer.java
@@ -19,7 +19,7 @@ public class BenchServer {
     private static final String URL_PARAMETER_NAME = "url";
     private static final String COUNT_PARAMETER_NAME = "count";
     private static final String COUNT_DEFAULT_VALUE = "10";
-    private static final Duration timeout = Duration.ofMillis(5000);
+    private static final Duration TIMEOUT = Duration.ofMillis(5000);
 
     private ActorRef cache;
 
@@ -39,7 +39,7 @@ public class BenchServer {
                 })
                 .mapAsync(1, benchRequest -> { //TODO: check parallelism parameter
                     CompletionStage<Object> stage = Patterns.ask(cache, benchRequest, timeout);
-                    
+                    timeout.
 
                 });
     }
