6a7b8f8d69248f16a4848fc103b004be264d4a97
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-10 23:46:46 +0300

    Scripted auto-commit on change (2019-11-10 23:46:46) by gitwatch.sh

--- src/main/java/ru/labs/benchmarker/BenchServer.java
+++ src/main/java/ru/labs/benchmarker/BenchServer.java
@@ -68,9 +68,7 @@ public class BenchServer {
                     cache.tell(benchResult, ActorRef.noSender());
                     return httpBenchResponse(benchResult);
                 })
-                .recover(new PFBuilder()
-                        .match(NumberFormatException.class, ex -> "recover")
-                        .build());
+                ;
     }
 
     private CompletionStage<BenchResult> benchExecuteStage(BenchRequest benchRequest, ActorMaterializer materializer) {
