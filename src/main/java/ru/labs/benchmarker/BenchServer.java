a5350d7e2516dc821f0a36875548bcf62e27bca7
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-10 19:25:38 +0300

    Scripted auto-commit on change (2019-11-10 19:25:38) by gitwatch.sh

--- src/main/java/ru/labs/benchmarker/BenchServer.java
+++ src/main/java/ru/labs/benchmarker/BenchServer.java
@@ -1,8 +1,14 @@
 package ru.labs.benchmarker;
 
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
 import ru.labs.benchmarker.actors.CacheActor;
 
 public class BenchServer {
-    private CacheActor cache;
+    private ActorRef cache;
+
+    public BenchServer(ActorSystem system) {
+        ActorRef
+    }
 
 }
