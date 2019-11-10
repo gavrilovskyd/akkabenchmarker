bb5fbbcb74083df131c4a38b293a2ef23d6d9ce8
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-10 18:05:57 +0300

    Scripted auto-commit on change (2019-11-10 18:05:57) by gitwatch.sh

--- src/main/java/ru/labs/benchmarker/BenchmarkerApp.java
+++ src/main/java/ru/labs/benchmarker/BenchmarkerApp.java
@@ -2,12 +2,13 @@ package ru.labs.benchmarker;
 
 import akka.actor.ActorSystem;
 import akka.http.javadsl.Http;
+import akka.stream.ActorMaterializer;
 
 public class BenchmarkerApp {
     public static void main(String[] args) throws Exception {
         ActorSystem system = ActorSystem.create("benchmarker-system");
 
         final Http http = Http.get(system);
-        final 
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
     }
 }
