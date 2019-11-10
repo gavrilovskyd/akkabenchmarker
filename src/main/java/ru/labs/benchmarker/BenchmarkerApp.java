c9031d0530e9ff9c7be2ddaa1b09b8b4924377bf
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-10 20:39:33 +0300

    Scripted auto-commit on change (2019-11-10 20:39:33) by gitwatch.sh

--- src/main/java/ru/labs/benchmarker/BenchmarkerApp.java
+++ src/main/java/ru/labs/benchmarker/BenchmarkerApp.java
@@ -2,11 +2,15 @@ package ru.labs.benchmarker;
 
 import akka.NotUsed;
 import akka.actor.ActorSystem;
+import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
+
+import java.util.concurrent.CompletionStage;
 
 public class BenchmarkerApp {
     public static void main(String[] args) throws Exception {
@@ -16,6 +20,7 @@ public class BenchmarkerApp {
         final ActorMaterializer materializer = ActorMaterializer.create(system);
         BenchServer server = new BenchServer(system);
         final Flow<HttpRequest, HttpResponse, NotUsed> httpFlow = server.flow(system, materializer);
-        
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                httpFlow, ConnectHttp.toHost("localhost", 8080), materializer);
     }
 }
