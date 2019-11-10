6b7a0465a34b02b58896a731d6411dfbc937e995
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-10 19:27:09 +0300

    Scripted auto-commit on change (2019-11-10 19:27:08) by gitwatch.sh

--- src/main/java/ru/labs/benchmarker/BenchServer.java
+++ src/main/java/ru/labs/benchmarker/BenchServer.java
@@ -1,8 +1,12 @@
 package ru.labs.benchmarker;
 
+import akka.NotUsed;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.stream.javadsl.Flow;
 import ru.labs.benchmarker.actors.CacheActor;
 
 public class BenchServer {
@@ -12,4 +16,5 @@ public class BenchServer {
         this.cache = system.actorOf(Props.create(CacheActor.class), "cache");
     }
 
+    public Flow<HttpRequest, HttpResponse, NotUsed> flow()
 }
