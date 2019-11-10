e28b4777255d162863b322f68a50bec6404c164c
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-10 19:10:38 +0300

    Scripted auto-commit on change (2019-11-10 19:10:38) by gitwatch.sh

--- src/main/java/ru/labs/benchmarker/actors/CacheActor.java
+++ src/main/java/ru/labs/benchmarker/actors/CacheActor.java
@@ -2,6 +2,7 @@ package ru.labs.benchmarker.actors;
 
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
+import ru.labs.benchmarker.messages.GetFromCacheMessage;
 
 import java.util.HashMap;
 
@@ -11,7 +12,9 @@ public class CacheActor extends AbstractActor {
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create()
-
+                .match(GetFromCacheMessage.class, m -> {
+                    
+                })
                 .build();
     }
 }
