440ab2af736a6e57a51b610fc7d985508dace677
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-10 19:43:15 +0300

    Scripted auto-commit on change (2019-11-10 19:43:15) by gitwatch.sh

--- src/main/java/ru/labs/benchmarker/BenchServer.java
+++ src/main/java/ru/labs/benchmarker/BenchServer.java
@@ -34,7 +34,7 @@ public class BenchServer {
                 })
                 .mapAsync(params -> {
                     if (params.) {
-                        
+
                     }
                 });
     }
