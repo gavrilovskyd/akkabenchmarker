da150122285742facca690b1a2a252fa67cafd02
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-15 11:53:13 +0300

    Scripted auto-commit on change (2019-11-15 11:53:13) by gitwatch.sh

--- src/main/java/ru/labs/benchmarker/core/BenchServer.java
+++ src/main/java/ru/labs/benchmarker/core/BenchServer.java
@@ -87,7 +87,7 @@ public class BenchServer {
                 .mapConcat(benchRequest ->
                         Collections.nCopies(benchRequest.getCount(), benchRequest.getURL())
                 )
-                .mapAsync(count, url -> { // benchRequest.getCount()
+                .mapAsync(count, url -> {
                     long start = System.nanoTime();
                     return httpClient
                             .prepareGet(url)
